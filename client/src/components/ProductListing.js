a610f5cfd65d91b700934699fd0caab591331d6c
Asmit shrivastava <asmitshrivastava8@gmail.com>
2021-03-25 01:02:38 +0530

    added sorting and fixed wishlist

--- client/src/components/ProductListing.js
+++ client/src/components/ProductListing.js
@@ -3,7 +3,7 @@ import { productContext } from '../contexts/ProductContext';
 
 const ProductListing = () => {
 
-    const {products,dispatch} = useContext(productContext);
+    const {products,dispatch,sortBy,includeOutOfStock,fastDelivery} = useContext(productContext);
 
     const addToWishlist = (item) => {
         if( item.isWishlist ){
@@ -14,18 +14,36 @@ const ProductListing = () => {
     }
 
     return (
-        <div className="products">
+        <div>
+            <fieldset>
+                <legend>Sort by :</legend>
+                <label htmlFor="sort">
+                    <input type="radio" checked ={ sortBy === "LOW_TO_HIGH" } onChange={() => dispatch({ type:"SORT",payload:"LOW_TO_HIGH" })} name="sort"/>Low to High
+                    <input type="radio" checked ={ sortBy === "HIGH_TO_LOW" } onChange={() => dispatch({ type:"SORT",payload:"HIGH_TO_LOW" })} name="sort"/>High to Low
+                </label>
+            </fieldset>
+
+            <fieldset>
+                <legend>Available :</legend>
+                    <input type="checkbox" checked = {includeOutOfStock} onChange={() => dispatch({ type:"INCLUDE_OUT_STOCK" })}/>Include Out Of Stock
+                    <input type="checkbox" checked = {fastDelivery} onChange={() => dispatch({ type:"FAST_DELIVERY" })}/>Fast Delivery
+            </fieldset>
+            
+            <div className="products">
             {
                 products.map( item => (
                     <div key={item.id} className="card">
                       <i onClick={() => addToWishlist(item)} style={{ color:item.isWishlist ? 'red' : 'white'}} className="fa fa-heart" aria-hidden="true"></i>
                       <img alt="product" className="card-img" src={item.image}/>
+                      <h3>{item.name}</h3>
                       <p>Price:{item.price}</p>
                       <p>{ item.inStock ? "Instock" : "out of stock" }</p>
+                      <p>{ item.fastDelivery ? "Fast Delivery available" : ""}</p>
                       <button className="btn" disabled={!item.inStock}>Add to cart</button>
                     </div>
                 ))
             }
+            </div>
         </div>
     );
 };
--- client/src/contexts/ProductContext.js
+++ client/src/contexts/ProductContext.js
@@ -1,5 +1,5 @@
 import React, { createContext,useReducer,useContext } from 'react';
-import { products } from '../productsDb';
+import { productsDb } from '../productsDb';
 
 export const productContext = createContext();
 
@@ -7,6 +7,12 @@ export const ProductContextProvider = ({children}) => {
     
     const productsReducer = (state,action) => {
         switch(action.type){
+            case "SORT":
+            return {...state,sortBy:action.payload};
+            case "INCLUDE_OUT_STOCK":
+            return {...state,includeOutOfStock:!state.includeOutOfStock};
+            case "FAST_DELIVERY":
+            return {...state,fastDelivery:!state.fastDelivery};
             case "ADDTOWISHLIST":
             return { ...state,products:state.products.map( i => i.id === action.id ? {...i,isWishlist:true} : i ) }
             case "REMOVEFROMWISHLIST":
@@ -16,12 +22,27 @@ export const ProductContextProvider = ({children}) => {
         }
     }
 
-    const [state,dispatch] = useReducer(productsReducer,{products});
+    const [state,dispatch] = useReducer(productsReducer,{products:productsDb,sortBy:null,includeOutOfStock:true,fastDelivery:false});
+
+    const getSortedProducts = (state) => {
+        if( state.sortBy === "LOW_TO_HIGH" ) return {...state,products:state.products.sort( (a,b) => a.price - b.price)};
+        if( state.sortBy === "HIGH_TO_LOW" ) return {...state,products:state.products.sort( (a,b) => b.price - a.price)};
+        return state;
+    }
+
+    const getFilteredProducts = (sortedProducts) => {
+        return {...sortedProducts, products:sortedProducts.products.filter( p => sortedProducts.includeOutOfStock ? true : p.inStock)
+        .filter( p => sortedProducts.fastDelivery ? p.fastDelivery : true )}
+    }
+
+    const sortedProducts = getSortedProducts(state);
+    const {includeOutOfStock,products,sortBy,fastDelivery} = getFilteredProducts(sortedProducts); 
 
     const wishlist = state.products.filter( i => i.isWishlist);
+    console.log(wishlist);
 
     return(
-        <productContext.Provider value={{ products:state.products, dispatch,wishlist }}>
+        <productContext.Provider value={{ includeOutOfStock,products,sortBy, dispatch,wishlist,fastDelivery }}>
             {children}
         </productContext.Provider>
     );
