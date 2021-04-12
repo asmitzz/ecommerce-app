c977476e6dfa8285b38ef9f40406fc53cd655662
Asmit shrivastava <asmitshrivastava8@gmail.com>
2021-04-12 18:39:01 +0530

    added express server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const port = 8000;
+
+app.use(bodyParser.json());
+app.use(cors);
+
+app.listen(port,() => console.log("server listening on port : ",port))
