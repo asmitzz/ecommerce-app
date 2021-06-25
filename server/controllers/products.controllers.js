1b141b2f64c35529d8040d6718ade25dde05df99
Asmit shrivastava <asmitshrivastava8@gmail.com>
2021-06-25 23:02:54 +0530

    added individual product get api

--- server/controllers/products.controllers.js
+++ server/controllers/products.controllers.js
@@ -1,8 +1,17 @@
 const Product = require('../models/product.model');
 
+const getProduct = async (req, res) => {
+   const { productId } = req.params;
+   const product = await Product.findById(productId);
+   if(product){
+      return res.status(200).json({ product  })
+   }
+   res.status(404).json({ message:"product not found" })
+}
+
 const getProducts = async(req, res) => {
    const products = await Product.find({});
    res.json({products})
 }
 
-module.exports = { getProducts };
+module.exports = { getProducts,getProduct };
--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts } = require('../controllers/products.controllers.js')
+const { getProducts,getProduct } = require('../controllers/products.controllers.js')
 
+router.get('/products/:productId',getProduct)
 router.get('/products',getProducts)
 
 module.exports = router;
